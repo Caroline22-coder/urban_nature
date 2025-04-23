ce88fbbdf2e919d673781e59355adea8088c90d4
Caroline21-coder <caroline.cesselin@ucd.ie>
2025-04-23 18:23:40 +0200

    adding something but problem not working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View, ScrollView } from "react-native";
+import { Image, Text, View, ScrollView, ActivityIndicator } from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
@@ -23,14 +23,28 @@ export default function Index() {
           <Image source={images.bg} className="absolute w-full z-0" /> 
           <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" /> 
+                {moviesLoading ? (
+                  <ActivityIndicator
+                    size="large"
+                    color="#0000ff"
+                    className="mt-10 self-center"
 
-            
-            <View className="flex-1 mt-5">
-              <SearchBar 
-                  onPress={() => router.push("/search")}
-                  placeholder= "Search for a movie"
+                  />
+                ) : moviesError ? (
+                  <Text> Error: {moviesError?.message} </Text>
+                ) : ( 
+                <View className="flex-1 mt-5">
+                    <SearchBar 
+                          onPress={() => router.push("/search")}
+                          placeholder= "Search for a movie"
               /> 
-            </View>
+
+              <> 
+                  <Text className= "test-lg text-white font-bold mt-5 mb-3"> Latest Movies </Text>
+              
+              </>
+                    </View>
+                )}
 
           </ScrollView>
     </View>
