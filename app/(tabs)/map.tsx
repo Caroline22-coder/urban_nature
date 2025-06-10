5089870e2b588b6359b9ecfc3fc4c3425bb717e7
Caroline21-coder <caroline.cesselin@ucdconnect.ie>
2025-06-10 18:38:48 +0200

    added the biodiversity map successfully

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,83 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
-import MapView, { Marker, Callout } from 'react-native-maps';
-import * as Location from 'expo-location';
-import { useSpeciesAnalysis } from './speciesAnalysis';
 
-const MyMapApp = () => {
-  const [userLocation, setUserLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-  const [loading, setLoading] = useState(true);
-  const { analyses } = useSpeciesAnalysis();
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-  useEffect(() => {
-    (async () => {
-      let location = await Location.getCurrentPositionAsync({});
-      setUserLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
-      setLoading(false);
-    })();
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
-  if (!userLocation) {
-    return (
-      <View style={styles.center}>
-        <MapView style={styles.map} />
-      </View>
-    );
-  }
-
+export default function MapWebView() {
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: userLocation.latitude,
-          longitude: userLocation.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      >
-    
-        {/* Analysed species markers */}
-        {analyses.map((item) => (
-          <Marker
-            key={item.id}
-            coordinate={item.location}
-            title={item.analysis.common_name}
-            description={item.analysis.scientific_name}
-            pinColor="blue"
-          >
-           
-          </Marker>
-        ))}
-      </MapView>
+      <WebView
+        source={{ uri: 'https://ucdirelandeu.maps.arcgis.com/apps/mapviewer/index.html?webmap=50abb98efab44bf0bbca27031c3dc81a'}}
+                        style={{ flex: 1 }}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
     flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  center: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
-
-export default MyMapApp;
