ef219f4e2338204881db4f5365ca98138e348846
Caroline21-coder <caroline.cesselin@ucdconnect.ie>
2025-06-20 17:00:36 +0200

    changed background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Stack, Slot } from "expo-router";
 import './globals.css';
 import {SpeciesAnalysisProvider} from "./(tabs)/speciesAnalysis";
 
--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ export default function Welcome() {
 
   return (
     <ImageBackground
-      source={require("../assets/images/hortensias.jpg")}
+      source={require("../assets/images/newphoto.jpg")}
       style={styles.background}
       resizeMode="cover"
     >
