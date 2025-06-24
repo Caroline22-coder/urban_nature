e37b6035f4296644e72c5e5556c405df32ce41ab
Caroline21-coder <caroline.cesselin@ucdconnect.ie>
2025-06-24 13:18:19 +0200

    put a different background in picture.tsx

--- app/(tabs)/picture.tsx
+++ app/(tabs)/picture.tsx
@@ -150,7 +150,7 @@ export default function App() {
     await Sharing.shareAsync(fileUri);
   };
 
-  const localImage = require('../../assets/images/rose.jpg');
+  const localImage = require('../../assets/images/newimage.jpg');
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container} edges={['left', 'right']}>
