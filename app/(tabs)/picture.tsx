9d4d00bfd361c48ff783575fbe240f7c5a23aa18
Caroline22-coder <caroline.cesselin@centrale-med.fr>
2025-06-30 15:20:20 +0200

    setting everything up to release the app

--- app/(tabs)/picture.tsx
+++ app/(tabs)/picture.tsx
@@ -124,7 +124,7 @@ export default function App() {
     } as any);
 
     try {
-      const response = await fetch("http://192.168.195.177:5000/analyze", {
+      const response = await fetch("https://backend-cxwq.onrender.com/analyze", {
         method: "POST",
         headers: {
           "Content-Type": "multipart/form-data",
